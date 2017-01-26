444c9183b7f4effd60231f585ce6e3f749426ce2
lwei0402 <marsakamarshall@gmail.com>
2017-01-26 00:45:18 -0800

    Add files via upload

---
+++ EE183_LAB1_IK.m
@@ -0,0 +1,48 @@
+function EE183_LAB1_IK(x,y,z)
+theta1 = 0;
+theta2 = 0;
+theta3 = 0;
+theta4 = 0;
+
+alpha0 = 0;
+alpha1 = 90;
+alpha2 = -90;
+alpha3 = 0;
+d = 0.3;
+
+    for theta1 = 0:5:90
+        for theta2 = 0:5:90
+            for theta3 = 0:5:90
+                for theta4 = 0:5:90
+A1 = [cosd(theta1),-sind(theta1),0,0;
+      sind(theta1)*cosd(alpha0),cosd(theta1)*cosd(alpha0),-sind(alpha0),0;
+      sind(theta1)*sind(alpha0),cosd(theta1)*sind(alpha0),cosd(alpha0),0;
+      0,0,0,1;];
+A2 = [cosd(theta2),-sind(theta2),0,0;
+      sind(theta2)*cosd(alpha1),cosd(theta2)*cosd(alpha1),-sind(alpha1),0;
+      sind(theta2)*sind(alpha1),cosd(theta2)*sind(alpha1),cosd(alpha1),0;
+      0,0,0,1;];
+A3 = [cosd(theta3),-sind(theta3),0,0.3;
+      sind(theta3)*cosd(alpha2),cosd(theta3)*cosd(alpha2),-sind(alpha2),0;
+      sind(theta3)*sind(alpha2),cosd(theta3)*sind(alpha2),cosd(alpha2),0;
+      0,0,0,1;];
+A4 = [cosd(theta4),-sind(theta4),0,0;
+      sind(theta4)*cosd(alpha3),cosd(theta4)*cosd(alpha3),-sind(alpha3),0;
+      sind(theta4)*sind(alpha3),cosd(theta4)*sind(alpha3),cosd(alpha3),0;
+      0,0,0,1;];
+
+A = A1*A2*A3*A4;
+if abs(x - A(1,4))< 0.01 && abs(y - A(2,4))<0.01&& abs(z-A(3,4))<0.01
+    display(acosd(A1(1,1)));
+    display(acosd(A2(1,1)));
+    display(acosd(A3(1,1)));
+    display(acosd(A4(1,1)));
+    return
+end
+    
+                end
+            end
+        end
+    end
+end
+%disp(A);
